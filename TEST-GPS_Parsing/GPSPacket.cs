3e7af3b4271783f7f749ec12b06da01bdfb33d34
MattDN93 <matthew.deneef@gmail.com>
2016-10-04 17:44:30 +0200

    Added checksum calculation method
    Method compares the received checksum with the calculated one to check for data integrity.

--- TEST-GPS_Parsing/GPSPacket.cs
+++ TEST-GPS_Parsing/GPSPacket.cs
@@ -40,6 +40,9 @@ namespace TEST_GPS_Parsing
         //friendly status flags return
         public string fixtype_f;
         public string fixqual_f;
+        
+        //checksum for integrity
+        public checksum;
 
         //Default constructor
         public GPSPacket()
@@ -66,6 +69,7 @@ namespace TEST_GPS_Parsing
             //friendly status flags return
             fixtype_f = "";
             fixqual_f = "";
+            checksum = "";
         }
 
         //function to return friendly string from flags
@@ -103,6 +107,28 @@ namespace TEST_GPS_Parsing
         public string getFriendlyFixQual()
         {
             return fixqual_f;
+        }
+        
+        //Make sure the checksum is correct by generating a checksum from the string and comparing
+        private static string getChecksum(string sentence)
+        {
+          //Start with first Item
+          int checksumCalc= Convert.ToByte(sentence[sentence.IndexOf('$')+1]);
+          // Loop through all chars to get a checksum
+          for (int i=sentence.IndexOf('$')+2 ; i<sentence.IndexOf('*') ; i++)
+          {
+            // No. XOR the checksum with this character's value
+            checksumCalc^=Convert.ToByte(sentence[i]);              
+          }
+          // Return the checksum formatted as a two-character hexadecimal
+          if (checksumCalc.ToString() == checksum)
+          {
+              return true;
+          }else 
+          {
+              return false;
+          }
+
         }
 
                 //-------------------------PARSING MEMBER METHODS----------------------------
